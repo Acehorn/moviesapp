cadda8bc573099f3ad20ebcc07174093db15f76c
acehorn <mecatronicastillo@gmail.com>
2026-01-31 22:58:19 -0600

    feat(Firebase): firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:moviesapp/features/presentation/pages/characters_page.dart';
+import 'package:moviesapp/firebase_options.dart';
 import 'features/characters/data/futurama_api.dart';
 import 'features/characters/data/omdb_api.dart';
 import 'features/characters/store/character_store.dart';
@@ -7,6 +9,9 @@ import 'package:moviesapp/core/storage/favorites_storage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   final store = CharacterStore(
     FuturamaApi(),
@@ -14,6 +19,7 @@ void main() async {
     FavoritesStorage(),
   );
 
+ 
   await store.init();
 
   runApp(MyApp(store: store));
